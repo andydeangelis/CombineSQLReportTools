84db5043ce04db0bc3862bf9f73d85d252cfebcf
andydeangelis <andy.deangelis@live.com>
2018-05-16 10:45:40 -0400

    Version 1.2
    Updated with both a GUI and ability to run from command line with parameters.

--- Functions/Get-ClusterResource.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Get-ClusterResource
-{
-  param($cluster)
-    gwmi -ComputerName $cluster -Authentication PacketPrivacy -Namespace "root\mscluster" -Class MSCluster_Resource | add-member -pass NoteProperty Cluster $cluster | 
-    add-member -pass ScriptProperty Node `
-    { gwmi -namespace "root\mscluster" -computerName $this.Cluster -Authentication PacketPrivacy -query "ASSOCIATORS OF {MSCluster_Resource.Name='$($this.Name)'} WHERE AssocClass = MSCluster_NodeToActiveResource" | Select -ExpandProperty Name } |
-    add-member -pass ScriptProperty Group `
-    { gwmi -ComputerName $this.Cluster -Authentication PacketPrivacy -Namespace "root\mscluster" -query "ASSOCIATORS OF {MSCluster_Resource.Name='$($this.Name)'} WHERE AssocClass = MSCluster_ResourceGroupToResource" | Select -ExpandProperty Name }
-}
--- SQLScripts/SQLVersionInfo.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-            SERVERPROPERTY('MachineName') AS [ServerName], 
-			SERVERPROPERTY('ServerName') AS [ServerInstanceName], 
-            SERVERPROPERTY('InstanceName') AS [Instance], 
-            SERVERPROPERTY('Edition') AS [Edition],
-            SERVERPROPERTY('ProductVersion') AS [ProductVersion], 
-			Left(@@Version, Charindex(' - ', @@version) - 1) As VersionName
